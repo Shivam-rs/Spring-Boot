8cfb5720e8ad23f8e53dba0721881072b8fda60f
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-09-22 23:55:30 -0400

    Till Actuators

--- learning-spring-boot-fw/src/main/java/com/learningspringbootfw/webcourses/controller/WebCourseController.java
+++ learning-spring-boot-fw/src/main/java/com/learningspringbootfw/webcourses/controller/WebCourseController.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.learningspringbootfw.webcourses.bean.WebCourse;
 
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
 @RestController
 public class WebCourseController {
 //http://localhost:8080/courses
 	@GetMapping("/courses")
 
 	public List<WebCourse> getAllCourses() {
-		return List.of(new WebCourse(1, "Turning Pro", "Shivam"));
+		return List.of(new WebCourse(1, "Turning Pro", "Shivam"),
+				new WebCourse(2, "Turning noob", "Rishu") );
+	}
+		@GetMapping("/courses/1")
+
+		public WebCourse getOneCourses() {
+			return new WebCourse(1, "Turning Pro", "Shivam");
 
 	}
 }
